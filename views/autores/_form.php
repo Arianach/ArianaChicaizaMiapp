1206509693e689dd300d1f585cb278c8e616b9da
Arianach <ariana.chicaiza.parrales@utelvt.edu.ec>
2026-06-04 15:12:39 -0500

    Rediseño UI completo + migración de Usuarios a User + nuevo módulo UserController

--- views/autores/_form.php
+++ views/autores/_form.php
@@ -20,6 +20,6 @@ use yii\widgets\ActiveForm;
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
 
-    <?php ActiveForm::end(); ?>
+    <?php ActiveForm::end(); ?>    
 
 </div>
